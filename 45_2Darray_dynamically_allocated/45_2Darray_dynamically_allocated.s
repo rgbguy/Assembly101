bb78d3f88eec7c53be684ff29428de76bd70ef18
rgbguy <sagarkalbande1993@gmail.com>
2025-08-01 06:13:55 +0530

    Dyanmic 2D format fixed

--- 45_2Darray_dynamically_allocated/45_2Darray_dynamically_allocated.s
+++ 45_2Darray_dynamically_allocated/45_2Darray_dynamically_allocated.s
@@ -29,183 +29,138 @@ main:
     pushl   %ebp
     movl    %esp,   %ebp
 
-    subl    $16,    %esp
-    subl    $4,     %esp                #space for ppPtr
+    subl    $20,    %esp        #local vars including ppPtr
+    movl    $0,     -20(%ebp)   #ppPtr = NULL
 
     pushl   $msg_main_enter_value_of_rowcol
     call    printf
     addl    $4, %esp
 
-    leal    -4(%ebp),   %ecx
-    leal    -8(%ebp),   %ebx
+    leal    -4(%ebp),   %ebx    #&iRows
+    leal    -8(%ebp),   %eax    #&iColumns
+    pushl   %eax
     pushl   %ebx
-    pushl   %ecx
     pushl   $msg_main_scan_rowcolval
     call    scanf
     addl    $12,    %esp
 
-    movl    -4(%ebp),   %eax
-    movl    $4, %ecx
-    mull    %ecx            #eax now has value iRows*4
+#malloc and nullcheck
+    movl    -4(%ebp),   %eax    #iRows
+    movl    $4, %ecx            #sizeof(int*)
+    mull    %ecx                #iRows * sizeof(int*) stored in eax
     pushl   %eax
-    call    malloc          #malloc will return adress in eax
+    call    malloc              #output of malloc stored in eax
     addl    $4, %esp
-    movl    %eax,   -20(%ebp)   #address stored in ppPtr
-
+    movl    %eax,   -20(%ebp)   #output of malloc stored in ppPtr
     movl    $0, %edx
-    cmpl    %eax,   %edx
-    je      label_memfailed
+    cmpl    %eax,   %edx        #NULLCHECK
+    je      label_memfail
 
-    #for11 starts
-    movl    $0, -12(%ebp)
+#for11
+    movl    $0, -12(%ebp)       #iCounter1 = 0
     jmp     label_for11_condition
 
-label_for11_condition:
-    movl    -4(%ebp),   %eax
-    movl    -12(%ebp),  %edx
-    cmpl    %eax    ,%edx
-    jge     label_for11_exit
-
-    movl    -8(%ebp),   %eax
-    movl    $4, %ecx
-    mull    %ecx            #eax now has value iCol*4
+label_for11:
+    movl    -8(%ebp),   %eax    #iColumns
+    movl    $4,         %ecx    #sizeof(int)
+    mull    %ecx                #iColumns * sizeof(int) -> output stored in eax
     pushl   %eax
-    call    malloc          #malloc will return adress in eax
+    call    malloc              #output of malloc stored in eax
     addl    $4, %esp
-    movl    -20(%ebp),  %ebx
-    movl    -12(%ebp),  %ecx
-    movl    %eax,   (%ebx, %ecx, 4)   #address stored in ppPtr[iCounter1]
 
+    movl    -20(%ebp),  %ebx    #ppPtr
+    movl    -12(%ebp),  %ecx    #iCounter1  
+    movl    %eax,   (%ebx, %ecx, 4) #malloc output stored in ppPtr[iCounter1]
     movl    $0, %edx
-    cmpl    %eax,   %edx
-    je      label_memfailed
+    cmpl    %eax,   %edx        #NULLCHECK
+    je      label_memfail
 
-        #for12 starts
-        movl    $0, -16(%ebp)
+    #for12
+        movl    $0, -16(%ebp)       #iCounter2 = 0
         jmp     label_for12_condition
 
-        label_for12_condition:
-        movl    -8(%ebp),   %eax
-        movl    -16(%ebp),  %edx
-        cmpl    %eax,   %edx
-        jge     label_increment_iCounter11
-
-        pushl   -16(%ebp)
-        pushl   -12(%ebp)
+    label_for12:
+        pushl   -16(%ebp)       #iCounter2
+        pushl   -12(%ebp)       #iCounter1
         pushl   $msg_main_enter_each_value
         call    printf
         addl    $12,    %esp
 
-        movl    -20(%ebp),  %ebx
-        movl    -12(%ebp),  %ecx
-        movl    (%ebx, %ecx, 4),    %ebx
-
-        movl    -16(%ebp),  %ecx
-        leal    (%ebx, %ecx, 4),    %ebx
-
+        movl    -20(%ebp),  %ebx    #ppPtr
+        movl    -12(%ebp),  %ecx    #iCounter1  
+        movl    (%ebx, %ecx, 4),    %ebx    #ppPtr[iCounter1]
+        movl    -16(%ebp),  %ecx    #iCounter2 
+        leal    (%ebx, %ecx, 4),    %ebx     #&ppPtr[iCounter1][iCounter2]
         pushl   %ebx
         pushl   $msg_main_scanf_each_value
         call    scanf
         addl    $8, %esp
 
-        addl    $1, -16(%ebp)
+        addl    $1,     -16(%ebp)   #increment iCounter2
 
-        jmp     label_for12_condition
+    label_for12_condition:
+        movl    -16(%ebp),  %eax    #iCounter2
+        movl    -8(%ebp),   %edx    #iColumns
+        cmpl    %edx,   %eax        #iCounter2 < iColumns
+        jl      label_for12
 
-label_increment_iCounter11:
-    addl    $1, -12(%ebp)
-    jmp     label_for11_condition
+    addl    $1,     -12(%ebp)   #increment iCounter1
+label_for11_condition:
+    movl    -12(%ebp),  %eax    #iCounter1
+    movl    -4(%ebp),   %edx    #iRows
+    cmpl    %edx,   %eax        #iCounter1 < iRows
+    jl      label_for11
 
-
-label_for11_exit:
-
-#============================================================
-
+#===========================================================================
     pushl   $msg_main_entered_elements_are
     call    printf
     addl    $4, %esp
+#===========================================================================
 
-    #for21 starts
-    movl    $0, -12(%ebp)
+#for21
+    movl    $0, -12(%ebp)       #iCounter1 = 0
     jmp     label_for21_condition
 
-label_for21_condition:
-    movl    -4(%ebp),   %eax
-    movl    -12(%ebp),  %edx
-    cmpl    %eax    ,%edx
-    jge     label_for21_exit
-
-        #for22 starts
-        movl    $0, -16(%ebp)
+label_for21:
+    #for22
+        movl    $0, -16(%ebp)       #iCounter2 = 0
         jmp     label_for22_condition
 
-        label_for22_condition:
-        movl    -8(%ebp),   %eax
-        movl    -16(%ebp),  %edx
-        cmpl    %eax,   %edx
-        jge     label_increment_iCounter21
-
-        movl    -20(%ebp),  %ebx
-        movl    -12(%ebp),  %ecx
-        movl    (%ebx, %ecx, 4),    %ebx
-
-        movl    -16(%ebp),  %ecx
-        movl    (%ebx, %ecx, 4),    %ebx
-
-        pushl   %ebx
-        pushl   -16(%ebp)
-        pushl   -12(%ebp)
+    label_for22:
+        movl    -20(%ebp),  %ebx        #base address of ppPtr
+        movl    -12(%ebp),  %eax        #iCounter1
+        movl    (%ebx, %eax, 4), %ebx   #ppPtr[iCounter1]
+        movl    -16(%ebp),  %eax        #iCounter2
+        movl    (%ebx, %eax, 4),    %eax
+        pushl   %eax
+        pushl   -16(%ebp)       #iCounter2
+        pushl   -12(%ebp)       #iCounter1
         pushl   $msg_main_print_each_value
         call    printf
-        addl    $16, %esp
+        addl    $16,    %esp
 
-        addl    $1, -16(%ebp)
+        addl    $1,     -16(%ebp)   #increment iCounter2
 
-        jmp     label_for22_condition
+    label_for22_condition:
+        movl    -16(%ebp),  %eax    #iCounter2
+        movl    -8(%ebp),   %edx    #iColumns
+        cmpl    %edx,   %eax        #iCounter2 < iColumns
+        jl      label_for22
 
-label_increment_iCounter21:
-    addl    $1, -12(%ebp)
-    jmp     label_for21_condition
+    addl    $1,     -12(%ebp)   #increment iCounter1
+label_for21_condition:
+    movl    -12(%ebp),  %eax    #iCounter1
+    movl    -4(%ebp),   %edx    #iRows
+    cmpl    %edx,   %eax        #iCounter1 < iRows
+    jl      label_for21
 
-#============================================================
-
-#free for starts
-    movl    $0, -12(%ebp)
-
-label_forFREE_condition:
-    movl    -4(%ebp),   %eax
-    movl    -12(%ebp),  %edx
-    cmpl    %eax    ,%edx
-    jge     label_forFREE_exit
-
-    movl    -20(%ebp),  %ebx
-    movl    -12(%ebp),  %ecx
-    movl    (%ebx, %ecx, 4),    %ebx
-    pushl   %ebx
-    call    free
-    addl    $4, %esp
-
-    movl    -20(%ebp),  %ebx
-    movl    -12(%ebp),  %ecx
-    movl    $0, (%ebx, %ecx, 4)
-
-    addl    $1, -12(%ebp)
-    jmp     label_forFREE_condition
-
-label_forFREE_exit:
-    pushl   -20(%ebp)
-    call    free
-    addl    $4, %esp
-
-label_for21_exit:
     pushl   $0
     call    exit
-
-label_memfailed:
+    
+label_memfail:
     pushl   $msg_main_memfailed
     call    puts
     addl    $4, %esp
-
     pushl   $-1
     call    exit
     
